42a34429cc88322d33e190b544e11f00c1e55d43
Rakesh <rakeshkumar.akuluru@gmail.com>
2026-01-28 15:08:27 -0600

    adding converstations shell

---
+++ converstation.sh
@@ -0,0 +1,4 @@
+echo "Suresh:: Hello Ram, How are you?"
+echo "Ram:: Hi Suresh, I am fine. How about you?"
+echo "Suresh:: I am doing well too. What are your plans for today?"
+echo "Ram:: I was thinking of going for a hike. Would you like to join me"
