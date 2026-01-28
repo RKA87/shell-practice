b54c5d588b3011bae9cac27381a46760860e1800
Rakesh <rakeshkumar.akuluru@gmail.com>
2026-01-28 15:25:56 -0600

    updated with variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1="suresh"
+PERSON2="Ram"
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine. How about you?"
+echo "$PERSON1:: I am doing well too. What are your plans for today?"
+echo "$PERSON2:: I was thinking of going for a hike. Would you like to join me"
+echo "$PERSON1:: That sounds great! I would love to join you."
