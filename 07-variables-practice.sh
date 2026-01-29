1bb02cd1e604495e0fde0144474c48784111235d
Rakesh <rakeshkumar.akuluru@gmail.com>
2026-01-29 16:31:43 -0600

    new file for practice

--- 07-variables-practice.sh
+++ 07-variables-practice.sh
@@ -3,10 +3,19 @@
 PERSON1="Alice"
 PERSON2="Bob"
 
-PERSON3=$1 
-PERSON4=$2
-
 echo "Hello, $PERSON1 and $PERSON2!"
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine. How about you?"
+echo "$PERSON1:: I am doing well too. What are your plans for today?"
+echo "$PERSON2:: I was thinking of going for a hike. Would you like to join me"
+echo "$PERSON1:: That sounds great! I would love to join you."
+
+
+PERSON3=$1 #dynamical enter the values as an arguments
+PERSON4=$2 #dynamical enter the values as an arguments
 
 echo "Test Check dynamically enter the values as a arguments while executing the shell; Hello, $PERSON3 and $PERSON4!"
+echo "Finally Data Output as expected with $PERSON3 and $PERSON4"
+
+
 
