cd442e2556bb1f7745e57291906abf72c820e4ef
Rakesh <rakeshkumar.akuluru@gmail.com>
2026-01-29 17:36:55 -0600

    new file for practice

--- 08-executed_output_into_variable.sh
+++ 08-executed_output_into_variable.sh
@@ -1,5 +1,16 @@
 #/bin/bash
+START_TIMESTAMP=$(date +%s)
 
-COMMAND_OUTPUT=$(ls -l)
+echo "Script started Timestamp:$START_TIMESTAMP"
 
+COMMAND_OUTPUT=$(ls -l /tmp)
 echo "The output of the command is: $COMMAND_OUTPUT"
+
+sleep 20
+
+END_TIMESTAMP=$(date +%s)
+echo "Script Ended Timestamp: $END_TIMESTAMP"
+
+TOTAL_TIME=$(($END_TIMESTAMP - $START_TIMESTAMP))
+
+echo "Total Execution Time: $TOTAL_TIME seconds"
