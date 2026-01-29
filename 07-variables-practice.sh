f05611eb4f5b3994b558987c369f6f7014a908bb
Rakesh <rakeshkumar.akuluru@gmail.com>
2026-01-29 16:22:38 -0600

    new file for practice

--- 07-variables-practice.sh
+++ 07-variables-practice.sh
@@ -3,4 +3,10 @@
 PERSON1="Alice"
 PERSON2="Bob"
 
+PERSON3=$1 
+PERSON4=$2
+
 echo "Hello, $PERSON1 and $PERSON2!"
+
+echo "Test Check dynamically enter the values as a arguments while executing the shell; Hello, $PERSON3 and $PERSON4!"
+
