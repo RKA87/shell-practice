59a58756f25d83f9564a0d61685855a901277b0a
Rakesh <rakeshkumar.akuluru@gmail.com>
2026-01-31 09:56:06 -0600

    shell practice

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -3,10 +3,10 @@ NUM=$1
 
 if [ $NUM -gt 20 ]; then
   echo "The number is greater than 20."
-  elif [ $NUM -eq 20 ]; then
+elif [ $NUM -eq 20 ]; then
   echo "The number is equal to 20."
-  elif [ $NUM -lt 20 ]; then
+elif [ $NUM -lt 20 ]; then
   echo "The number is less than 20."
-  else
+ else
   echo "It is Invalid number."
   fi
