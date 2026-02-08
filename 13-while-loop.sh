d9a63c972ba2906e3b52ae014b1ed57c47649b67
Rakesh <rakeshkumar.akuluru@gmail.com>
2026-02-08 17:36:20 -0600

    while loop

--- 13-while-loop.sh
+++ 13-while-loop.sh
@@ -8,3 +8,20 @@ for each_file in $directory_path
 do
     echo "File Name: $each_file"
 done
+
+
+#Now want this to be use it as function
+
+directory_path_log() {
+
+    directory_path=$1
+
+    echo "File Directory Path: $directory_path"
+
+    for each_file in $directory_path/*;
+    do
+        echo "File Name: $each_file"
+    done
+}
+
+directory_path_log "/home/ec2-user/shell-practice/*"
