9ca122fd82df7f87e0fcee6f0685244e653ef9e2
Rakesh <rakeshkumar.akuluru@gmail.com>
2026-02-08 18:24:48 -0600

    while loop

--- 15-while-loop-using-IFS-Read.sh
+++ 15-while-loop-using-IFS-Read.sh
@@ -2,7 +2,7 @@
 LOG_DIR=$1
 FILES_FROM_DIR=$(find $LOG_DIR/* -name "*.sh")
 
-echo "Files loaded into variable, it will print the files below one by one with 3 seconds sleep time
+echo "Files loaded into variable, it will print the files below one by one with 3 seconds sleep time"
 
 while IFS= read -r file; do
     echo "Each FileName: $file"
