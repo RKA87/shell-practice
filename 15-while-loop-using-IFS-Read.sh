4ed7226a052040f0d32d5774da7d2b5165f3a14e
Rakesh <rakeshkumar.akuluru@gmail.com>
2026-02-08 18:18:23 -0600

    while loop

--- 15-while-loop-using-IFS-Read.sh
+++ 15-while-loop-using-IFS-Read.sh
@@ -3,3 +3,9 @@
 FILES_FROM_DIR=$(find $1/* -name "*.sh")
 
 echo "Files from Directory: $FILES_FROM_DIR"
+
+while IFS= read -r file; do
+    echo "Each FileName: $file"
+    sleep 3
+    echo "-----------------------------"
+done <<< "$FILES_FROM_DIR"
