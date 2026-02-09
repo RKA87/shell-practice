664598a945af8b58a3d7608fd787e788b5e1d79d
Rakesh <rakeshkumar.akuluru@gmail.com>
2026-02-08 18:08:01 -0600

    while loop

--- 14-while-loop-using-variable_for_path-files-print.sh
+++ 14-while-loop-using-variable_for_path-files-print.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-LOG_DIR_VARIABLE=$(find "/home/ec2-user/shell-practice/" -name "*.sh" )
+function_check(){
+    LOG_DIR_VARIABLE=$(find $1/* -name "*.sh")
+    #Now LOG_DIR_VARIABLE is a variable that contains the path of all the .sh files in the directory
+    echo "File Directory Path Files List: $LOG_DIR_VARIABLE"
+}
 
-#/home/ec2-user/shell-practice/
 
-#Now LOG_DIR_VARIABLE is a variable that contains the path of all the .sh files in the directory
+function_check "/home/ec2-user/shell-practice"
 
-echo "File Directory Path Files List: $LOG_DIR_VARIABLE"
+
+
+
